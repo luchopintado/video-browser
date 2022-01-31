06367664cbc99b116ca9cf6df75fbb23330890fc
Luis <luchopintado@gmail.com>
2022-01-31 15:14:18 -0500

    fix vulnerabilities

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDNAF-yOyZP1z2wd-FK3xfChbaB0hKvZ88';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
